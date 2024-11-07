506158c3cddbfff060dfb33e8514e6bd34df2bdf
Alexandre Neuville <alexandre.neuville@nubios.be>
2024-11-07 16:49:16 +0100

    Remove sudo (directly in root on the machine)

--- aws/scripts/install.sh
+++ aws/scripts/install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-sudo yum update -y
-sudo yum install -y docker
-sudo service docker start
-sudo usermod -a -G docker ec2-user
-sudo docker pull nginx:latest
-sudo docker run --name nginx-server -p 80:80 -d nginx
+yum update -y
+yum install -y docker
+service docker start
+usermod -a -G docker ec2-user
+docker pull nginx:latest
+docker run --name nginx-server -p 80:80 -d nginx
